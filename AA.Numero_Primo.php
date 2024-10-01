0e6e9192d448774c27bf83265f53d7a4e9fbf826
Angel David <zadosesp@hotmail.com>
2024-10-01 13:48:06 +0200

    AA.1

---
+++ AA.Numero_Primo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php 
+            function prime ($number) {
+                $counter = 0;
+                for ($i=$number; $i > 0 ; $i--) { 
+                    if ($number % $i == 0) {
+                        $counter++;
+                    }
+                }
+                if ($counter == 2) {
+                    return "Es primo";
+                } else {
+                    return "No es primo";
+                }
+            }
+            echo prime(2);
+        ?>
+    </body>
+</html>
